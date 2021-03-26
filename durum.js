cd81b7f5d81facf50f838a32b789959f2f24531f
omerusatpro <53378106+omerusatpro@users.noreply.github.com>
2021-03-27 00:10:28 +0300

    Update durum.js

--- durum.js
+++ durum.js
@@ -4,14 +4,14 @@ client.on('ready', () => {
 client.request('SET_ACTIVITY', {
 pid: process.pid,
 activity : {
-details : "Youtube kanalıma abone olmayı unutma", // BAŞLIĞIN ALTINDAKİ YAZI
+details : "", // BAŞLIĞIN ALTINDAKİ YAZI
 assets : {
-large_image : "omerusatpro", // FOTOĞRAF (EN KÜÇÜK 512 X 512 EN BÜYÜK 1024x1024)
-large_text : "Youtube kanalıma abone olmayı unutma" // BAŞLIK İSMİ 
+large_image : "", // FOTOĞRAF (EN KÜÇÜK 512 X 512 EN BÜYÜK 1024x1024)
+large_text : "" // BAŞLIK İSMİ 
 },
-buttons : [{label : "Youtube kanalım" , url : "https://www.youtube.com/channel/UCV46IJQfpX3OA0VwHgdyzAg"},{label : "Discord sunucum", url : "https://discord.gg/yn84YmUyPQ"}]
+buttons : [{label : "" , url : ""},{label : "", url : ""}]
 }
 })
 })
 console.log(`Durumunuz Başariyla Ayarlandi.`)
-client.login({ clientId : "814184864353222657" }).catch(console.error);
+client.login({ clientId : "" }).catch(console.error);
